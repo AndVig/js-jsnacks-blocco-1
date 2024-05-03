0d82457d2d53779091917b558a2ad55caaccaefd
AndVig <andrea.vigna2@gmail.com>
2024-05-03 11:46:05 +0200

    fine primo script

---
+++ JSnack2/JS/script.js
@@ -0,0 +1,16 @@
+var parola1= prompt('inserisci prima parola');
+var parola2= prompt('inserisci (seconda parola');
+
+for(var i=0; i<parola1.length;i++){
+
+}
+for(var j=0; j<parola2.length;j++){
+    
+}
+if(i>j){
+    console.log(parola1)
+    console.log(parola2)
+}else{
+    console.log(parola2)
+    console.log(parola1)
+}
