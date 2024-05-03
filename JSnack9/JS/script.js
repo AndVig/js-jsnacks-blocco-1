a4cb2477c93702196bcfaa22a6dc948f051e0128
AndVig <andrea.vigna2@gmail.com>
2024-05-03 19:02:54 +0200

    fine JS9

---
+++ JSnack9/JS/script.js
@@ -0,0 +1,12 @@
+//variabili
+var num= 10;
+var somma=0;
+var media;
+//ciclo per la somma
+for(let i=0;1<num;i++){
+    somma+=i;
+    console.log(somma);
+}
+//calcolo della media
+media=somma/num;
+console.log(media);
