ea6cfd7e8058c92e5b7946662dcd844a15983d92
AndVig <andrea.vigna2@gmail.com>
2024-05-03 19:10:25 +0200

    fine script9

--- JSnack9/JS/script.js
+++ JSnack9/JS/script.js
@@ -1,12 +1,13 @@
 //variabili
-var num= 10;
-var somma=0;
-var media;
+let numb= 10;
+let somma=0;
+let media;
+console.log(numb);
 //ciclo per la somma
-for(let i=0;1<num;i++){
-    somma+=i;
+for(let i=1;i<numb+1;i++){
+    somma=somma+i;
     console.log(somma);
 }
 //calcolo della media
-media=somma/num;
+media=somma/numb;
 console.log(media);
